42d0bd74f712549ab4af8698a1ffadb168f0154c
abbyclarke <clarkeab@oregonstate.edu>
2022-03-07 23:18:55 -0500

    working on readval procedure

--- Proj6_clarkeab.asm
+++ Proj6_clarkeab.asm
@@ -17,7 +17,7 @@ INCLUDE Irvine32.inc
 ;
 ; Preconditions:
 ;
-; Receives: prompt (Offset of a prompt), userString (offset of empty string), size (length of string- input value), bytesRead 
+; Receives: prompt (Offset of a prompt), userString (offset of empty string), size (length of string- input value), bytesRead (offset)
 ;
 ; Returns:
 ; -------------------------------------------------------------------
@@ -27,12 +27,12 @@ mGetString	MACRO prompt, userString, size, bytesRead
   push	EDX
   push	EDI
   
-  mov	EDX, OFFSET prompt
+  mov	EDX, prompt
   call	WriteString
   mov	ECX, size
-  mov	EDX, OFFSET userString
+  mov	EDX, userString
   call	ReadString
-  mov	EDI, OFFSET bytesRead
+  mov	EDI, bytesRead
   mov	[EDI], ECX
 
   pop	EDI
@@ -56,7 +56,7 @@ mDisplayString	MACRO	printString
   push	EAX
   push	EDX
 
-  mov	EDX, OFFSET printString
+  mov	EDX, printString
   call	WriteString
 
   pop EDX
@@ -70,6 +70,10 @@ prompt1		BYTE	"Please enter a signed number: ", 0
 string1		BYTE	11 DUP(?)
 sMax		DWORD	11
 sLength		DWORD	?
+newInt		SDWORD	?
+errorMsg	BYTE	"ERROR: You did not enter a signed number of your number was too big.", 13,10
+			BYTE	"Please try again: ", 0
+
 
 
 ; (insert variable definitions here)
@@ -77,13 +81,100 @@ sLength		DWORD	?
 .code
 main PROC
   
-  mGetString	prompt1, string1, sMax, sLength
-  call	CrLf
-  mDisplayString	string1	
+  push	OFFSET errorMsg
+  push	OFFSET newInt
+  push	OFFSET prompt1
+  push	OFFSET string1
+  push	sMax
+  push	OFFSET sLength
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
-; (insert additional procedures here)
+
+; ------------------------------------------------------------
+; name: ReadVal
+
+; description: 
+;
+; preconditions: ebp+8 = Offset sLength, ebp+12 = sMax, ebp+16 = offset string1, ebp+20=offset prompt1, ebp+24=offset newInt,
+; ebp+28= offset errorMsg, ebp+32=
+;
+; postconditions: 
+;
+; receives: 
+;
+; returns: 
+; ---------------------------------------------------------------
+ReadVal PROC
+  LOCAL	signFlag: DWORD, multiply: DWORD			; 0 for positive, 1 for negative
+  mov	signFlag, 0
+  mov	multiply, 1									; will increase by 10s
+  mGetString	[EBP + 20], [EBP + 16], [EBP + 12], [EBP + 8]
+  
+_start_over:  
+  mov	ECX, [EBP + 8]
+  mov	ESI, [EBP + 16]
+  add	ESI, ECX
+  dec	ESI
+  mov	EDI, [EBP + 24]
+  mov	EBX, multiply
+
+_start:
+  STD
+  LODSB
+  cmp	AL, 48
+  jl	_check_sign
+  cmp	AL, 57
+  jg	_invalid
+  sub	AL, 48
+  mov	EBX, multiply		; multiply by decimal place one, ten, hundred, etc
+  mul	EBX
+  mov	[EDI], EAX				
+
+
+_check_sign:
+  cmp	ESI, 0
+  jne	_invalid
+  cmp	AL, 45
+  jne	_check_pos
+  mov	signFlag, 1
+
+_check_pos:
+  cmp	AL, 43
+  jne	_invalid
+  mov	signFlag, 0
+
+_invalid:
+  mGetString	[EBP + 28], [EBP + 16], [EBP + 12], [EBP + 8]
+  jmp	_start_over
+
+
+
+  
+  ret
+ReadVal ENDP
+
+; ------------------------------------------------------------
+; name: WriteVal
+;
+; description: 
+;
+; preconditions: 
+;
+; postconditions: 
+;
+; receives: 
+;
+; returns: 
+; ---------------------------------------------------------------
+WriteVal PROC
+  push	EBP
+  mov	EBP, ESP
+
+
+  pop	EBP
+  ret
+WriteVal ENDP
 
 END main
